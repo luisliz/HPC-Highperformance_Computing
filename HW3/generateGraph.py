aaaf5321b19f6a4703a185a87a7a475fb4c9537e
Luis Liz <luis.liz@upr.edu>
2019-10-21 12:18:36 -0400

    Python graph gen ready

--- HW3/generateGraph.py
+++ HW3/generateGraph.py
@@ -1,39 +1,18 @@
 # Author: Aric Hagberg (hagberg@lanl.gov)
 import matplotlib.pyplot as plt
 import networkx as nx
-import random 
+import random
+import math 
 
 n = 100
-nodes = []
-
+randW = []
 for i in range(n): 
-    nodes.append(i)
-G = nx.Graph()
+    randW.append(random.random())
 
-rWeight = random.random()
-G.add_edge('a', 'b', weight=rWeight)
-G.add_edge('a', 'c', weight=0.2)
-G.add_edge('c', 'd', weight=0.1)
-G.add_edge('c', 'e', weight=0.7)
-G.add_edge('c', 'f', weight=0.9)
-G.add_edge('a', 'd', weight=0.3)
+G = nx.fast_gnp_random_graph(n,0.1)
 
-elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 0.5]
-esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= 0.5]
+nx.draw(G)
 
-pos = nx.spring_layout(G)  # positions for all nodes
-
-# nodes
-nx.draw_networkx_nodes(G, pos, node_size=700)
-
-# edges
-nx.draw_networkx_edges(G, pos, edgelist=elarge,
-        width=6)
-nx.draw_networkx_edges(G, pos, edgelist=esmall,
-        width=6, alpha=0.5, edge_color='b', style='dashed')
-
-# labels
-nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
-
-plt.axis('off')
-plt.show()
+for i in G.edges(data=True): 
+    i[2]['weight'] = random.random()
+    print(i)
