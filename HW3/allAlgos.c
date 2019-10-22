9017fd1c4593c5993054663e3089975a6c93180a
luisliz <luis.liz@upr.edu>
2019-10-22 19:58:22 -0400

    added time measurement

--- HW3/allAlgos.c
+++ HW3/allAlgos.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <time.h> 
 
 #define INF 99999
 #define MAX 500 
@@ -9,9 +10,7 @@ void printDistances(int dist[MAX][MAX], int n) { printf("\nShortest distances be
 
 				for(int i = 0; i<n; i++) {
 								for(int j=0; j< n; j++) {
-												if(dist[i][j]==INF)
-																printf("INF\t"); 
-												else 
+												if(dist[i][j]==INF) printf("INF\t"); else 
 																printf("%d\t", dist[i][j]); 
 								}
 								printf("\n"); 
@@ -35,60 +34,112 @@ void readFromArray(int graph[MAX][MAX], int n) {
 }
 
 //SHORTEST PATH ALGORITHMS 
-void dijsk(int graph[MAX][MAX], int n) { 
-				int dist[MAX][MAX]; 
-				int sptSet[MAX][MAX]; 
+void dijsk(int graph[MAX][MAX], int n, int startnode) { 
+				int cost[MAX][MAX],distance[MAX],pred[MAX];
+				int visited[MAX],count,mindistance,nextnode,i,j;
 
-				for (int i = 0; i < n; i++) 
-								dist[i][i] = INF, sptSet[i][i] = 0; 
+				for(i=0;i<n;i++)
+								for(j=0;j<n;j++)
+												if(graph[i][j]==0)
+																cost[i][j]=INF;
+												else
+																cost[i][j]=graph[i][j];
 
-				for (int src = 0; src < n; src++)  {
-								dist[src][src] = 0; 
-
-								for (int count = 0; count < n - 1; count++) { 
-												int min = INF, min_index; 
-												for (int v = 0; v < n; v++) 
-																if (sptSet[count][v] == 0 && dist[count][v] <= min) 
-																				min = dist[count][v], min_index = v; 
-
-												int u = min_index; 
-
-												sptSet[count][u] = 1; 
-
-												for (int v = 0; v < n; v++) 
-																if (!sptSet[count][v] && graph[u][v] && dist[count][u] != INF && dist[count][u] + graph[u][v] < dist[count][v]) 
-																				dist[count][v] = dist[count][u] + graph[u][v]; 
-								} 
+				for(i=0;i<n;i++)
+				{
+								distance[i]=cost[startnode][i];
+								pred[i]=startnode;
+								visited[i]=0;
 				}
-				printDistances(dist, n); 
+
+				distance[startnode]=0;
+				visited[startnode]=1;
+				count=1;
+
+				while(count<n)
+				{
+								mindistance=INF;
+
+								//nextnode gives the node at minimum distance
+								for(i=0;i<n;i++)
+												if(distance[i]<mindistance&&!visited[i])
+												{
+																mindistance=distance[i];
+																nextnode=i;
+												}
+
+								//check if a better path exists through nextnode			
+								visited[nextnode]=1;
+								for(i=0;i<n;i++)
+												if(!visited[i])
+																if(mindistance+cost[nextnode][i]<distance[i])
+																{
+																				distance[i]=mindistance+cost[nextnode][i];
+																				pred[i]=nextnode;
+																}
+								count++;
+				}
+
+			/*for(int i = 0; i < n; i++) {
+							if(distance[i]==INF)
+											printf("INF\t");
+							else 
+								printf("%d\t",distance[i]); 
+			}*/				 
 }
 
-void parDijsk(int graph[MAX][MAX], int n) { 
-				int dist[MAX][MAX]; 
-				int sptSet[MAX][MAX]; 
+void parDijsk(int graph[MAX][MAX], int n, int startnode) { 
+				int cost[MAX][MAX],distance[MAX],pred[MAX];
+				int visited[MAX],count,mindistance,nextnode,i,j;
 
-				for (int i = 0; i < n; i++) 
-								dist[i][i] = INF, sptSet[i][i] = 0; 
+				for(i=0;i<n;i++)
+								for(j=0;j<n;j++)
+												if(graph[i][j]==0)
+																cost[i][j]=INF;
+												else
+																cost[i][j]=graph[i][j];
 
-				for (int src = 0; src < n; src++)  {
-								dist[src][src] = 0; 
-
-								for (int count = 0; count < n - 1; count++) { 
-												int min = INF, min_index; 
-												for (int v = 0; v < n; v++) 
-																if (sptSet[count][v] == 0 && dist[count][v] <= min) 
-																				min = dist[count][v], min_index = v; 
-
-												int u = min_index; 
-
-												sptSet[count][u] = 1; 
-
-												for (int v = 0; v < n; v++) 
-																if (!sptSet[count][v] && graph[u][v] && dist[count][u] != INF && dist[count][u] + graph[u][v] < dist[count][v]) 
-																				dist[count][v] = dist[count][u] + graph[u][v]; 
-								} 
+				for(i=0;i<n;i++)
+				{
+								distance[i]=cost[startnode][i];
+								pred[i]=startnode;
+								visited[i]=0;
 				}
-				printDistances(dist, n); 
+
+				distance[startnode]=0;
+				visited[startnode]=1;
+				count=1;
+
+				while(count<n)
+				{
+								mindistance=INF;
+
+								//nextnode gives the node at minimum distance
+								for(i=0;i<n;i++)
+												if(distance[i]<mindistance&&!visited[i])
+												{
+																mindistance=distance[i];
+																nextnode=i;
+												}
+
+								//check if a better path exists through nextnode			
+								visited[nextnode]=1;
+								for(i=0;i<n;i++)
+												if(!visited[i])
+																if(mindistance+cost[nextnode][i]<distance[i])
+																{
+																				distance[i]=mindistance+cost[nextnode][i];
+																				pred[i]=nextnode;
+																}
+								count++;
+				}
+
+			/*for(int i = 0; i < n; i++) {
+							if(distance[i]==INF)
+											printf("INF\t");
+							else 
+								printf("%d\t",distance[i]); 
+			}*/				 
 }
 
 void floydAlgo(int graph[MAX][MAX], int n) {
@@ -112,7 +163,7 @@ void floydAlgo(int graph[MAX][MAX], int n) {
 												}
 								}
 				}
-				printDistances(distances, n); 
+				//printDistances(distances, n); 
 }
 
 void parFloydAlgo(int graph[MAX][MAX], int n) {
@@ -136,23 +187,49 @@ void parFloydAlgo(int graph[MAX][MAX], int n) {
 												}
 								}
 				}
-				printDistances(distances, n); 
+				//printDistances(distances, n); 
 }
 
 int main (int argc, char *argv[]) {
 				int n = atoi(argv[1]); 
 				int graph[MAX][MAX] = {0}; 
+				clock_t t; 
+				double time_taken; 
 				readFromArray(graph, n); 
-
-				printf("\nParallel Running Floyd's Algo"); 
-				parFloydAlgo(graph, n); 
-				printf("\nParallel Running Dijkstra's Algo"); 
-				parDijsk(graph, n);
 				
-				/*printf("\nRunning Floyd's Algo"); 
+				printf("\nFloyd's Algo"); 
+				t = clock(); 	
 				floydAlgo(graph, n); 
-				printf("\nRunning Dijkstra's Algo"); 
-				dijsk(graph, n);*/
+				t = clock()-t; 
+				time_taken = ((double)t)/CLOCKS_PER_SEC; 
+				printf("\n\tSingle took: %f seconds", time_taken);
+
+				t = clock(); 	
+				parFloydAlgo(graph, n); 
+				t = clock()-t; 
+				time_taken = ((double)t)/CLOCKS_PER_SEC; 
+				printf("\n\tParallel took: %f seconds\n", time_taken); 
+				
+				
+				printf("\nDijkstra's Algo"); 
+				t = clock(); 	
+				for(int i = 0; i < n; i++) {
+								dijsk(graph, n, i);
+				}
+				t = clock()-t; 
+				time_taken = ((double)t)/CLOCKS_PER_SEC; 
+				printf("\n\tSinlge took: %f seconds", time_taken);
+
+				t = clock(); 	
+				for(int i = 0; i < n; i++) {
+								parDijsk(graph, n, i);
+				}
+				t = clock()-t; 
+				time_taken = ((double)t)/CLOCKS_PER_SEC; 
+				printf("\n\tParallel took: %f seconds", time_taken);
+
+				
+				
 
 				return 0; 
 }
