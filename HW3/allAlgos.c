fc89a7371885285e436e4a234238334a14218679
luis <luis.liz@gmail.com>
2019-10-24 12:48:45 -0400

    asd

--- HW3/allAlgos.c
+++ HW3/allAlgos.c
@@ -10,127 +10,121 @@
 //HELPER FUNCTIONS 
 void printdistances(int **dist, int n) { printf("\nShortest distances between vertices\n"); 
 
-				for(int i = 0; i<n; i++) {
-								for(int j=0; j< n; j++) {
-												if(dist[i][j]==INF) printf("INF\t"); else 
-																printf("%d\t", dist[i][j]); 
-								}
-								printf("\n"); 
-				}
+	for(int i = 0; i<n; i++) {
+		for(int j=0; j< n; j++) {
+			if(dist[i][j]==INF) printf("INF\t"); else 
+				printf("%d\t", dist[i][j]); 
+		}
+		printf("\n"); 
+	}
 }
 
 void readFromArray(int **graph, int n) {
-				FILE *myFile; 
-				myFile = fopen("graph.txt", "r"); 
+	FILE *myFile; 
+	myFile = fopen("graph.txt", "r"); 
 
-				int i, j; 
+	int i, j; 
 
-				for(i =  0; i < n; i++) {
-								for(j = 0; j<n; j++) {
-												fscanf(myFile, "%d, ", &graph[i][j]); 
-								}
-				}
-				fclose(myFile); 
+	for(i =  0; i < n; i++) {
+		for(j = 0; j<n; j++) {
+			fscanf(myFile, "%d, ", &graph[i][j]); 
+		}
+	}
+	fclose(myFile); 
 }
 
 //SHORTEST PATH ALGORITHMS 
 void dijsk(int **graph, int n, int startnode) { 
-				int **cost,*distance, *pred;
-				int *visited;
-				int count,mindistance,nextnode,i,j;
+	int **cost,*distance, *pred;
+	int *visited;
+	int count,mindistance,nextnode,i,j;
 
-				cost = (int **) malloc(n*sizeof(int*)); 
-				for(int i = 0; i<n; i++)
-								cost[i] = (int *) malloc(n*sizeof(int)); 
+	cost = (int **) malloc(n*sizeof(int*)); 
+	for(int i = 0; i<n; i++)
+		cost[i] = (int *) malloc(n*sizeof(int)); 
 
-				distance = (int *) malloc(n*sizeof(int)); 
-				pred = (int *) malloc(n*sizeof(int)); 
-				visited = (int *) malloc(n*sizeof(int)); 
+	distance = (int *) malloc(n*sizeof(int)); 
+	pred = (int *) malloc(n*sizeof(int)); 
+	visited = (int *) malloc(n*sizeof(int)); 
 
-				for(i=0;i<n;i++)
-								for(j=0;j<n;j++)
-												if(graph[i][j]==0)
-																cost[i][j]=INF;
-												else
-																cost[i][j]=graph[i][j];
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			if(graph[i][j]==0)
+				cost[i][j]=INF;
+			else
+				cost[i][j]=graph[i][j];
 
-				for(i=0;i<n;i++) {
-								distance[i]=cost[startnode][i];
-								pred[i]=startnode;
-								visited[i]=0;
+	for(i=0;i<n;i++) {
+		distance[i]=cost[startnode][i];
+		pred[i]=startnode;
+		visited[i]=0;
+	}
+
+	distance[startnode]=0;
+	visited[startnode]=1;
+	count=1;
+
+	while(count<n) {
+		mindistance=INF;
+
+		//nextnode gives the node at minimum distance
+		for(i=0;i<n;i++)
+			if(distance[i]<mindistance&&!visited[i]) {
+				mindistance=distance[i];
+				nextnode=i;
+			}
+
+		//check if a better path exists through nextnode			
+		visited[nextnode]=1;
+		for(i=0;i<n;i++)
+			if(!visited[i])
+				if(mindistance+cost[nextnode][i]<distance[i]) {
+					distance[i]=mindistance+cost[nextnode][i];
+					pred[i]=nextnode;
 				}
-
-				distance[startnode]=0;
-				visited[startnode]=1;
-				count=1;
-
-				while(count<n) {
-								mindistance=INF;
-
-								//nextnode gives the node at minimum distance
-								for(i=0;i<n;i++)
-												if(distance[i]<mindistance&&!visited[i]) {
-																mindistance=distance[i];
-																nextnode=i;
-												}
-
-								//check if a better path exists through nextnode			
-								visited[nextnode]=1;
-								for(i=0;i<n;i++)
-												if(!visited[i])
-																if(mindistance+cost[nextnode][i]<distance[i]) {
-																				distance[i]=mindistance+cost[nextnode][i];
-																				pred[i]=nextnode;
-																}
-								count++;
-				}
-				/*for(int i = 0; i < n; i++) {
-					if(distance[i]==INF)
-					printf("INF\t");
-					else 
-					printf("%d\t",distance[i]); 
-					}*/	
+		count++;
+	}
 }
 
 void floydAlgo(int **graph, int n) {
-				int **distances, sum, i, j, k; 
+	int **distances, sum, i, j, k; 
 
-				distances = (int **) malloc(n*sizeof(int*)); 
-				for(int i = 0; i<n; i++)
-								distances[i] = (int *) malloc(n*sizeof(int));
+	distances = (int **) malloc(n*sizeof(int*)); 
+	for(int i = 0; i<n; i++)
+		distances[i] = (int *) malloc(n*sizeof(int));
 
-				for(i = 0; i<n; i++)
-								for(j=0; j< n; j++)
-												distances[i][j] = graph[i][j]; 
+	for(i = 0; i<n; i++)
+		for(j=0; j< n; j++)
+			distances[i][j] = graph[i][j]; 
 
-				for(k=0; k<n; k++) {
-								for(i = 0; i<n; i++) {
-												for(j=0; j < n; j++) {
-																if(i==j) 
-																				distances[i][j] = 0; 
-																else { 
-																				sum = distances[i][k]+distances[k][j]; 
-																				if(sum < distances[i][j]) {
-																								distances[i][j] = sum; 
-																				}
-																}
-												}
-								}
+	for(k=0; k<n; k++) {
+		for(i = 0; i<n; i++) {
+			for(j=0; j < n; j++) {
+				if(i==j) 
+					distances[i][j] = 0; 
+				else { 
+					sum = distances[i][k]+distances[k][j]; 
+					if(sum < distances[i][j]) {
+						distances[i][j] = sum; 
+					}
 				}
-				printdistances(distances, n); 
-				free(distances); 
+			}
+		}
+	}
+	printdistances(distances, n); 
+	free(distances); 
 }
 
 void parFloydAlgo(int **graph, int n) {
-				int **distances, k, i, j, sum;
+	int **distances, k, i, j, sum;
 
-				distances = (int **) malloc(n*sizeof(int*)); 
-				for(int i = 0; i<n; i++)
-								distances[i] = (int *) malloc(n*sizeof(int));
+	distances = (int **) malloc(n*sizeof(int*)); 
+	for(int i = 0; i<n; i++)
+		distances[i] = (int *) malloc(n*sizeof(int));
 
-				for(int i = 0; i<n; i++)
-								for(int j=0; j< n; j++)
-												distances[i][j] = graph[i][j]; 
+	for(int i = 0; i<n; i++)
+		for(int j=0; j< n; j++)
+			distances[i][j] = graph[i][j]; 
 
 	unsigned int lengthFromItoK, lengthFromItoKAndFromKToJ;
 	unsigned int numberOfVertices = n;
@@ -139,101 +133,105 @@ void parFloydAlgo(int **graph, int n) {
 	unsigned int initialRow, lastRow;
 	unsigned int initialColumn, lastColumn;
 	unsigned int threadNumber;
-				threadNumber = omp_get_thread_num();
+	int *kthRow = (int *) malloc(n*sizeof(int));
+	int *kthColumn = (int *) malloc(n*sizeof(int));
 
-				initialRow = (threadNumber / sqrtOfnumberOfThreads) * blockSize;
-				lastRow = initialRow + blockSize;
 
-				initialColumn = (threadNumber % sqrtOfnumberOfThreads) * blockSize;
-				lastColumn = initialColumn + blockSize;
+#pragma omp parallel 	
+	{
+		threadNumber = omp_get_thread_num();
+		printf("core %d\n", threadNumber); 
+		initialRow = (threadNumber / sqrtOfnumberOfThreads) * blockSize;
+		lastRow = initialRow + blockSize;
 
-								int *kthRow = (int *) malloc(n*sizeof(int));
-								int *kthColumn = (int *) malloc(n*sizeof(int));
-				for(k=0; k<n; k++) {
-								for (i = 0; i < numberOfVertices; ++i) {
-												kthRow[i] = distances[k][i];
-												kthColumn[i] = distances[i][k];
-													printf("%d - %d \n", kthRow[i], kthColumn[i]); 
-								}
-								for (i = initialRow; i < lastRow; ++i) {
-									lengthFromItoK = kthColumn[i];
-													printf("\t"); 
-									for (j = initialColumn; j < lastColumn; ++j) {
-													printf("%d ", j); 
-											if (i != j && i != k && j != k) {
-												lengthFromItoKAndFromKToJ = lengthFromItoK + kthRow[j];
-												unsigned int minLength = min(lengthFromItoKAndFromKToJ, distances[i][j]);
-												distances[i][j] = minLength;
-											}
-										}
-								}
-								//for(i = 0; i<n; i++) {
-								//				for(j=0; j < n; j++) {
-								//								//printf("%d - %d", omp_get_thread_num(), k);
-
-								//								if(i==j) 
-								//												distances[i][j] = 0; 
-								//								else { 
-								//												sum = distances[i][k]+distances[k][j]; 
-								//												if(sum < distances[i][j]) {
-								//																distances[i][j] = sum; 
-								//												}
-								//								}
-								//				}
-								//}
+		initialColumn = (threadNumber % sqrtOfnumberOfThreads) * blockSize;
+		lastColumn = initialColumn + blockSize;
+		for(k=0; k<n; k++) {
+			for (i = 0; i < numberOfVertices; ++i) {
+				kthRow[i] = distances[k][i];
+				kthColumn[i] = distances[i][k];
+			}
+			for (i = initialRow; i < lastRow; ++i) {
+				lengthFromItoK = kthColumn[i];
+				for (j = initialColumn; j < lastColumn; ++j) {
+					if (i != j && i != k && j != k) {
+						lengthFromItoKAndFromKToJ = lengthFromItoK + kthRow[j];
+						printf("%d - %d\n", i, j); 
+						unsigned int minLength = min(lengthFromItoKAndFromKToJ, distances[i][j]);
+						distances[i][j] = minLength;
+					}
 				}
+			}
+			//for(i = 0; i<n; i++) {
+			//				for(j=0; j < n; j++) {
+			//								//printf("%d - %d", omp_get_thread_num(), k);
 
-				printdistances(distances, n); 
-				free(distances); 
+			//								if(i==j) 
+			//												distances[i][j] = 0; 
+			//								else { 
+			//												sum = distances[i][k]+distances[k][j]; 
+			//												if(sum < distances[i][j]) {
+			//																distances[i][j] = sum; 
+			//												}
+			//								}
+			//				}
+			//}
+		}
+	}
+
+	printdistances(distances, n); 
+	free(distances); 
 }
 
 int main (int argc, char *argv[]) {
-				int n, **graph; 
-				clock_t t; 
-				double time_taken; 
+	int n, **graph; 
+	clock_t t; 
+	double time_taken; 
 
-				n = atoi(argv[1]);
+	n = atoi(argv[1]);
 
-				//Allocate all memory
-				graph = (int **) malloc(n*sizeof(int*)); 
-				for(int i = 0; i<n; i++)
-								graph[i] = (int *) malloc(n*sizeof(int)); 
+	//Allocate all memory
+	graph = (int **) malloc(n*sizeof(int*)); 
+	for(int i = 0; i<n; i++)
+		graph[i] = (int *) malloc(n*sizeof(int)); 
 
-				readFromArray(graph, n); 
+	readFromArray(graph, n); 
 
-				printf("\nFloyd's Algo"); 
-					clock_t begin = clock();
-					floydAlgo(graph, n); 
-					clock_t end = clock();
-					double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-					printf("\n\tSingle took: %f seconds", time_spent);
+	printf("\nFloyd's Algo"); 
+	clock_t begin = clock();
+	//floydAlgo(graph, n); 
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("\n\tSingle took: %f seconds\n", time_spent);
 
-				clock_t begin2 = clock();
-				parFloydAlgo(graph, n);
-				clock_t end2 = clock();
-				double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
-				printf("\n\tParallel took: %f seconds\n", time_spent2);
+	clock_t begin2 = clock();
+	//parFloydAlgo(graph, n);
+	clock_t end2 = clock();
+	double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
+	printf("\n\tParallel took: %f seconds\n", time_spent2);
 
 
-				/*printf("\nDijkstra's Algo"); 
-					t = clock(); 	
-					for(int i = 0; i < n; i++) {
-					dijsk(graph, n, i);
-					}
-					t = clock()-t; 
-					time_taken = ((double)t)/CLOCKS_PER_SEC; 
-					printf("\n\tSinlge took: %f seconds\n", time_taken);*/
+	printf("\nDijkstra's Algo"); 
+	t = clock(); 
+	/*for(int i = 0; i < n; i++) {
+		dijsk(graph, n, i);
+	}*/
+	t = clock()-t; 
+	time_taken = ((double)t)/CLOCKS_PER_SEC; 
+	printf("\n\tSinlge took: %f seconds\n", time_taken);
 
-				/*t = clock(); 	
-					for(int i = 0; i < n; i++) {
-					parDijsk(graph, n, i);
-					}
-					t = clock()-t; 
-					time_taken = ((double)t)/CLOCKS_PER_SEC; 
-					printf("\n\tParallel took: %f seconds\n", time_taken);*/
+	t = clock(); 
+	#pragma parallel for 
+		for(int i = 0; i < n; i++) {
+			dijsk(graph[i], n, i);
+		}
+	
+	t = clock()-t; 
+	time_taken = ((double)t)/CLOCKS_PER_SEC; 
+	printf("\n\tParallel took: %f seconds\n", time_taken);
 
-				free(graph);
-				return 0; 
+	free(graph);
+	return 0; 
 }
 
 
--- HW3/runner.sh
+++ HW3/runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 python3 generateGraph.py $1
-gcc -fopenmp allAlgos.c -o algos && ./algos $1 
+gcc -fopenmp allAlgos.c -lm -o algos && ./algos $1 
