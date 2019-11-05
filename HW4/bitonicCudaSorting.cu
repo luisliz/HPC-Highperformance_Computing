1a77c0dcee1a138e8a50c7d25021005f97882416
luisliz <luis.liz@upr.edu>
2019-11-05 19:21:43 -0400

    Added time checking

--- HW4/bitonicCudaSorting.cu
+++ HW4/bitonicCudaSorting.cu
@@ -7,90 +7,106 @@
 #define NUM_VALS THREADS*BLOCKS
 
 void rand_nums(int *values, int length) {
-    int i;
-    for (i = 0; i < length; ++i) {
-        values[i] = rand() % INT_MAX + 1;;
-    }
+				int i;
+				for (i = 0; i < length; ++i) {
+								values[i] = rand() % INT_MAX + 1;;
+				}
 }
 
 
 __global__ void bitonicMinorSort(int *innerValues, int j, int k) {
-    unsigned int i, ixj;
-    i = threadIdx.x + blockDim.x * blockIdx.x;
+				unsigned int i, ixj;
+				i = threadIdx.x + blockDim.x * blockIdx.x;
 
-    ixj = i ^ j;
+				ixj = i ^ j;
 
-    if ((ixj) > i) {
-        if ((i & k) == 0) {
-            if (innerValues[i] > innerValues[ixj]) {
-                int temp = innerValues[i];
-                innerValues[i] = innerValues[ixj];
-                innerValues[ixj] = temp;
-            }
-        }
+				if ((ixj) > i) {
+								if ((i & k) == 0) {
+												if (innerValues[i] > innerValues[ixj]) {
+																int temp = innerValues[i];
+																innerValues[i] = innerValues[ixj];
+																innerValues[ixj] = temp;
+												}
+								}
 
 
-        if ((i & k) != 0) {
-            if (innerValues[i] < innerValues[ixj]) {
-                int temp = innerValues[i];
-                innerValues[i] = innerValues[ixj];
-                innerValues[ixj] = temp;
-            }
-        }
-    }
+								if ((i & k) != 0) {
+												if (innerValues[i] < innerValues[ixj]) {
+																int temp = innerValues[i];
+																innerValues[i] = innerValues[ixj];
+																innerValues[ixj] = temp;
+												}
+								}
+				}
 }
 
 void bitonicSort(int *values, int n) {
-    int *innerValues;
+				int *innerValues;
 
-    size_t size = n * sizeof(int);
+				size_t size = n * sizeof(int);
 
-    cudaMalloc((void **) &innerValues, size);
-    cudaMemcpy(innerValues, values, size, cudaMemcpyHostToDevice);
+				cudaMalloc((void **) &innerValues, size);
+				cudaMemcpy(innerValues, values, size, cudaMemcpyHostToDevice);
 
-    int blockSize;
-    if(n < THREADS) {
-         blockSize = 1;
-    } else {
-        blockSize = ceil(n/THREADS);
-    }
+				int blockSize;
+				if(n < THREADS) {
+								blockSize = 1;
+				} else {
+								blockSize = ceil(n/THREADS);
+				}
 
-    printf("%d BLOCKS", blockSize);
-    dim3 blocks(blockSize, 1);
-    dim3 threads(THREADS, 1);
+				printf("%d BLOCKS", blockSize);
+				dim3 blocks(blockSize, 1);
+				dim3 threads(THREADS, 1);
 
 
 
-    int j, k;
+				int j, k;
 
-    for (k = 2; k <= n; k <<= 1) {
-        for (j = k >> 1; j > 0; j = j >> 1) {
-            bitonicMinorSort<<<blocks, threads>>> (innerValues, j, k);
-        }
-    }
+				for (k = 2; k <= n; k <<= 1) {
+								for (j = k >> 1; j > 0; j = j >> 1) {
+												bitonicMinorSort<<<blocks, threads>>> (innerValues, j, k);
+								}
+				}
 
-    cudaMemcpy(values, innerValues, size, cudaMemcpyDeviceToHost);
-    cudaFree(innerValues);
+				cudaMemcpy(values, innerValues, size, cudaMemcpyDeviceToHost);
+				cudaFree(innerValues);
 }
 
-int main() {
-    int *values = (int *) malloc(NUM_VALS * sizeof(int));
-    int *origValues = (int *) malloc(NUM_VALS * sizeof(int));
-    int k = 50;
-		int n = 2^k; 
-    rand_nums(values, n);
+int main(int argc, char *argv[]) {
+				int *values = (int *) malloc(NUM_VALS * sizeof(int));
+				int *origValues = (int *) malloc(NUM_VALS * sizeof(int));
+				int k = 10;  
+				if(argc==2) {
+								k = atoi(argv[1]); 
+								printf("%d\n", k); 
 
-    for (int i = 0; i < n; i++) {
-        origValues[i] = values[i];
-    }
+				} else {
+								printf("Please input a correct value for k. Default k = 10 will be used.\n"); 
+				}
 
-    bitonicSort(values, n);
-    cudaDeviceSynchronize();
-    printf("\nAfter:\n ");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t|%d\n ", values[i], origValues[i]);
-    }
-    printf("\n");
+				int n = pow(2,k); 
+				printf("%d\n", n); 
+				rand_nums(values, n);
+
+				for (int i = 0; i < n; i++) {
+								origValues[i] = values[i];
+				}
+				double time_spent = 0.0; 
+				clock_t begin = clock(); 
+
+				bitonicSort(values, n);
+				cudaDeviceSynchronize();
+				clock_t end = clock();
+
+				time_spent += (double)(end-begin) / CLOCKS_PER_SEC; 
+				printf("Elapsed time is %f seconds", time_spent); 
+				//printf("\nAfter:\n ");
+				//for (int i = 0; i < n; i++) {
+				//    printf("%d\t|%d\n ", values[i], origValues[i]);
+				//}
+				//printf("\n");
 }
+
 
 
