aea2926c3f8e97b43e7dc804407c9355fdff417e
Luis Liz <luis.liz@upr.edu>
2019-11-05 23:38:41 -0400

    Added serial bitonic

--- HW4/bitonicCudaSorting.cu
+++ HW4/bitonicCudaSorting.cu
@@ -7,100 +7,102 @@
 #define NUM_VALS THREADS*BLOCKS
 
 void rand_nums(int *values, unsigned long length) {
-	int i;
-	for (i = 0; i < length; ++i) {
-		values[i] = rand() % INT_MAX + 1;;
-	}
+				int i;
+				for (i = 0; i < length; ++i) {
+								values[i] = rand() % INT_MAX + 1;;
+				}
 }
 
 
 __global__ void bitonicMinorSort(int *innerValues, int j, int k) {
-	unsigned int i, ixj;
-	i = threadIdx.x + blockDim.x * blockIdx.x;
+				unsigned int i, ixj;
+				i = threadIdx.x + blockDim.x * blockIdx.x;
 
-	ixj = i ^ j;
+				ixj = i ^ j;
 
-	if ((ixj) > i) {
-		if ((i & k) == 0) {
-			if (innerValues[i] > innerValues[ixj]) {
-				int temp = innerValues[i];
-				innerValues[i] = innerValues[ixj];
-				innerValues[ixj] = temp;
-			}
-		}
+				if ((ixj) > i) {
+								if ((i & k) == 0) {
+												if (innerValues[i] > innerValues[ixj]) {
+																int temp = innerValues[i];
+																innerValues[i] = innerValues[ixj];
+																innerValues[ixj] = temp;
+												}
+								}
 
 
-		if ((i & k) != 0) {
-			if (innerValues[i] < innerValues[ixj]) {
-				int temp = innerValues[i];
-				innerValues[i] = innerValues[ixj];
-				innerValues[ixj] = temp;
-			}
-		}
-	}
+								if ((i & k) != 0) {
+												if (innerValues[i] < innerValues[ixj]) {
+																int temp = innerValues[i];
+																innerValues[i] = innerValues[ixj];
+																innerValues[ixj] = temp;
+												}
+								}
+				}
 }
 
 void bitonicSort(int *values, unsigned long n) {
-	int *innerValues;
+				int *innerValues;
 
-	size_t size = n * sizeof(int);
+				size_t size = n * sizeof(int);
 
-	cudaMalloc((void **) &innerValues, size);
-	cudaMemcpy(innerValues, values, size, cudaMemcpyHostToDevice);
+				cudaMalloc((void **) &innerValues, size);
+				cudaMemcpy(innerValues, values, size, cudaMemcpyHostToDevice);
 
-	int blockSize;
-	if(n < THREADS) {
-		blockSize = 1;
-	} else {
-		blockSize = ceil(n/THREADS);
-	}
+				int blockSize;
+				if(n < THREADS) {
+								blockSize = 1;
+				} else {
+								blockSize = ceil(n/THREADS);
+				}
 
-	dim3 blocks(blockSize, 1);
-	dim3 threads(THREADS, 1);
+				dim3 blocks(blockSize, 1);
+				dim3 threads(THREADS, 1);
 
-	int j, k;
+				int j, k;
 
-	for (k = 2; k <= n; k <<= 1) {
-		for (j = k >> 1; j > 0; j = j >> 1) {
-			bitonicMinorSort<<<blocks, threads>>> (innerValues, j, k);
-		}
-	}
+				for (k = 2; k <= n; k <<= 1) {
+								for (j = k >> 1; j > 0; j = j >> 1) {
+												bitonicMinorSort<<<blocks, threads>>> (innerValues, j, k);
+								}
+				}
 
-	cudaMemcpy(values, innerValues, size, cudaMemcpyDeviceToHost);
-	cudaFree(innerValues);
+				cudaMemcpy(values, innerValues, size, cudaMemcpyDeviceToHost);
+				cudaFree(innerValues);
 }
 
 int main(int argc, char *argv[]) {
-	int k = 10;  
+				int k = 10;  
 
-	if(argc==2)
-		k = atoi(argv[1]); 
+				if(argc==2)
+								k = atoi(argv[1]); 
 
-	int *values = (int *) malloc(NUM_VALS * sizeof(int));    
-	int *origValues = (int *) malloc(NUM_VALS * sizeof(int));    
-	
-	unsigned long n;
-	double time_spent;
-	clock_t begin, end; 
+				int *values = (int *) malloc(NUM_VALS * sizeof(int));    
+				int *origValues = (int *) malloc(NUM_VALS * sizeof(int));    
 
-	for(int i = 0; i < k; i++) {
-		n = pow(2,k); 
-		printf("\nk = %d, n = %ld\n", k, n); 
-		
-		rand_nums(values, n);
+				unsigned long n;
+				double time_spent;
+				clock_t begin, end; 
 
-		for (unsigned long i = 0; i < n; i++)
-			origValues[i] = values[i];
-		
+				n = pow(2,k); 
+				printf("\nk = %d, n = %ld\n", k, n); 
 
-		time_spent = 0.0; 
-		begin = clock(); 
-		bitonicSort(values, n);
-		end = clock();
-		time_spent += (double)(end-begin) / CLOCKS_PER_SEC; 
-		printf("\tElapsed time: %f seconds", time_spent); 
-	}
+				rand_nums(values, n);
+
+				for (unsigned long i = 0; i < n; i++)
+								origValues[i] = values[i];
+
+
+				time_spent = 0.0; 
+				begin = clock(); 
+				bitonicSort(values, n);
+				end = clock();
+				time_spent += (double)(end-begin) / CLOCKS_PER_SEC; 
+				printf("\tElapsed time: %f seconds\n", time_spent); 
+
+				free(values); 
+				free(origValues); 
 }
+
 
 
 
---
+++ HW4/bitonicSerialSorting.cu
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define THREADS 1024
+#define BLOCKS 65536
+#define NUM_VALS THREADS*BLOCKS
+#define ASCENDING 1
+#define DESCENDING 0
+
+void rand_nums(int *values, unsigned long length) {
+				int i;
+				for (i = 0; i < length; ++i) {
+								values[i] = rand() % INT_MAX + 1;;
+				}
+}
+
+void compAndSwap(int *a, int i, int j, int dir)
+{
+				int temp;
+				if (dir==(a[i]>a[j])) {
+								temp = a[j];
+								a[j] = a[i];
+								a[i] = temp;
+								//swap(a[i], a[j]);
+				}
+}
+
+void bitonicMerge(int *a, int low, int cnt, int dir) {
+				if (cnt > 1) {
+								int k = cnt / 2;
+								for (int i = low; i < low + k; i++)
+												compAndSwap(a, i, i + k, dir);
+								bitonicMerge(a, low, k, dir);
+								bitonicMerge(a, low + k, k, dir);
+				}
+}
+
+void bitonicSort(int *values, int low, unsigned long n, int dir) {
+				if(n>1) {
+								int k = n/2;
+
+								bitonicSort(values, low, k, ASCENDING);
+								bitonicSort(values, low+k, k, DESCENDING);
+
+								bitonicMerge(values, low, n, dir);
+				}
+
+}
+
+int main(int argc, char *argv[]) {
+				unsigned long n;
+				double time_spent;
+				clock_t begin, end;
+
+				int k = 10;
+				if(argc==2)
+								k = atoi(argv[1]);
+
+				n = pow(2,k);
+
+				int *values = (int *) malloc(NUM_VALS * sizeof(int));
+				int *origValues = (int *) malloc(NUM_VALS * sizeof(int));
+
+				printf("\nk = %d, n = %ld\n", k, n); 
+
+				rand_nums(values, n);
+
+				for (unsigned long i = 0; i < n; i++)
+								origValues[i] = values[i];
+
+				time_spent = 0.0; 
+				begin = clock(); 
+				bitonicSort(values, 0, n, ASCENDING);
+				end = clock();
+				time_spent += (double)(end-begin) / CLOCKS_PER_SEC; 
+				printf("\tElapsed time: %f seconds\n", time_spent); 
+
+				free(values); 
+				free(origValues); 
+}
+
+
+
+
