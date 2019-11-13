26c800b0efd84c966f574ceb2662f01dbca18879
Chameleon Cloud User <luis.liz@upr.edu>
2019-11-13 00:22:22 +0000

    Added multi processor tests

--- HW5/npb/run.sh
+++ HW5/npb/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh	
-./bt.C.x > bt.C.res.txt
-./sp.A.x > sp.A.res.txt
-./sp.B.x > sp.B.res.txt
-./sp.C.x > sp.C.res.txt
+mpiexec -np 24 bt.A.x > bt.A.res.txt
+mpiexec -np 24 bt.B.x > bt.B.res.txt
+mpiexec -np 24 bt.C.x > bt.C.res.txt
+mpiexec -np 24 sp.A.x > sp.A.res.txt
+mpiexec -np 24 sp.B.x > sp.B.res.txt
+mpiexec -np 24 sp.C.x > sp.C.res.txt
