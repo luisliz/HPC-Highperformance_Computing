c51cc77d95d18d746092caaabfaa223f9d9be242
Chameleon Cloud User <luis.liz@upr.edu>
2019-11-14 17:30:47 +0000

    Adding more test cases

--- HW5/npb/run.sh
+++ HW5/npb/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh	
-mpiexec -np 24 bt.A.x > bt.A.res.txt
-mpiexec -np 24 bt.B.x > bt.B.res.txt
-mpiexec -np 24 bt.C.x > bt.C.res.txt
-mpiexec -np 24 sp.A.x > sp.A.res.txt
-mpiexec -np 24 sp.B.x > sp.B.res.txt
-mpiexec -np 24 sp.C.x > sp.C.res.txt
+
+mkdir 2T
+echo "Created Dirs"
+
+mpiexec -np 2 bt.A.x > 1T/bt.A.res.txt
+mpiexec -np 2 bt.B.x > 1T/bt.B.res.txt
+mpiexec -np 2 bt.C.x > 1T/bt.C.res.txt
+mpiexec -np 2 sp.A.x > 1T/sp.A.res.txt
+mpiexec -np 2 sp.B.x > 1T/sp.B.res.txt
+mpiexec -np 2 sp.C.x > 1T/sp.C.res.txt
+
+echo "Finished 2"
