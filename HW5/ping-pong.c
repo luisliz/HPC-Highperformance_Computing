620ecd4d5376382415351a5d98376d2d9cdd3bac
luisliz <luis.liz@upr.edu>
2019-11-17 20:15:54 -0400

    added basic files

--- HW5/ping-pong.c
+++ HW5/ping-pong.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
 
-	const int PING_PONG_LIMIT = 10; 
+	const int PING_PONG_LIMIT = 2; 
 	MPI_Init(NULL, NULL); 
 	int world_rank; 
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); 
