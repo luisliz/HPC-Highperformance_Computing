8752c94ef751ea0c9c71e93d642706721b1df9b0
Luis Liz <luis.liz@upr.edu>
2019-11-20 18:41:02 -0400

    Simple scala counter

--- HW6/frequency.scala
+++ HW6/frequency.scala
@@ -3,29 +3,29 @@ import scala.collection.Map
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
-case class DoctTerms(filename:String, terms:Map[String, Int]) 
-
-class studyClass {
-  def tokenize(line: String): Array[String] = new WrappedString(line).toLowerCase().split(' ')
-
-  def study(filename:String):DocTerms = {
-    val conf = new SparkConf().setMaster("local[*").setAppName("simple-app")
-    val sc= new SparkContext(conf)
-
-    val counts = (sc.textFile(filename).flatMap(tokenize).map((s:String) => (s,1))
-      .reduceByKey( _ + _ )
-      .collectAsMap())
-    DocTerms(filename, counts)
-  }
-}
-
+//class DocTerms(filename:String, terms:Map[String, Int]) {
+//  val filename = filename
+//  val terms = terms
+//}
 object Main {
+  //def tokenize(line: String): Array[String] = new WrappedString(line).toLowerCase().split(' ')
 
+//  def study(filename:String):DocTerms = { 
+//    val conf = new SparkConf().setMaster("local[*]").setAppName("simple-app")
+//    val sc= new SparkContext(conf)
+//    
+//    val counts = (sc.textFile(filename).flatMap(tokenize).map((s:String) => (s,1))
+//        .reduceByKey( _ + _ )
+//        .collectAsMap())
+//    val dt = new DocTerms(filename, counts)
+//
+//    dt
+//  }
+//
   def main() {
-    val stc = new studyClass
-    val book1 = stc.study("/home/cc/Highperformance_Computing/HW6/sherlockShort.txt")
-
-    for(c<-book1.terms.slice(20)) println(c)
+    val text = sc.textFile("mytextfile.txt")
+ val counts = text.flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey(_+_) counts.collect
+    //for(c<-book1.terms.slice(20)) println(c)
   }
 
 }
