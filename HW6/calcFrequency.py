1ac22ebd8f7c7032bb7729b73c4b9f47ff03930d
Chameleon Cloud User <cc@llnightwatch.novalocal>
2019-11-20 04:19:24 +0000

    Working script

--- HW6/calcFrequency.py
+++ HW6/calcFrequency.py
@@ -8,12 +8,27 @@ conf.setAppName("Word count") #[*] is number of cores
 sc = SparkContext(conf=conf)
 sc.setLogLevel("WARN")
 
-rdd = sc.textFile("sherlockShort.txt")
+rdd = sc.textFile("sherlock.txt")
 
 docs = rdd.flatMap(lambda doc: doc.split(' '))
+res = docs.map(lambda word: (word, 1)).collect()
 
-print(docs.map(lambda word: (word, 1)).reduceByKey(add))
-#counts = sc.parallelize(textFile)
+print "Total count: " + str(docs.count())
+compact = {}
+for element, count in res:
+    if element in compact: 
+        compact[element] += count
+    else: 
+        compact[element] = count
 
-#sortd = rdd.flatMap(lambda word: [(word, 1)]).collect()
-#print(sortd)
+f = open("res.txt", "w+")
+for i in sorted(compact): 
+    strr = "%s: %d" % (i,  compact[i])
+    f.write(strr.encode('utf-8'))
+f.close()
+
+total = 0
+for element, count in res:
+    total += count
+
+print "Count original: " + str(docs.count()) + " total after process: " + str(total)
