66a0ffbca555f1993ea8c0dea19f832063732ca6
Chameleon Cloud User <cc@llnightwatch.novalocal>
2019-11-20 22:21:56 +0000

    fixing up scala

--- HW6/calcFrequency.py
+++ HW6/calcFrequency.py
@@ -1,35 +1,34 @@
 from pyspark import SparkContext, SparkConf 
 import timeit
+import threading
+
 def run_spark(): 
     conf = SparkConf()
-
-    conf.setMaster("local[8]")
     conf.setAppName("Word count") #[*] is number of cores
     sc = SparkContext(conf=conf)
-    
+
     rdd = sc.textFile("sherlock.txt")
-    
+
+    print "Runners: ", sc._conf.get('spark.executor.instances')
+
     docs = rdd.flatMap(lambda doc: doc.split(' '))
+
+    sc.parallelize(list(docs))
+
     res = docs.map(lambda word: (word, 1)).collect()
-    
+
     compact = {}
     for element, count in res:
         if element in compact: 
             compact[element] += count
         else: 
             compact[element] = count
-    
+
     f = open("res.txt", "w+")
     for i in sorted(compact): 
         strr = "%s: %d\n" % (i,  compact[i])
         f.write(strr.encode('utf-8'))
     f.close()
-    
-    total = 0
-    for element, count in res:
-        total += count
-    
-    print "Count original: " + str(docs.count()) + " total after process: " + str(total)
 
 elapsed_time = timeit.timeit(run_spark, number=1)
 print "Elapsed time", elapsed_time
--- HW6/frequency.scala
+++ HW6/frequency.scala
@@ -1,18 +1,31 @@
 import scala.collection.immutable.WrappedString; 
 import scala.collection.Map
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
-def tokenize(line: String): Array[String] = 
-  new WrappedString(line).toLowerCase().split(' ')
+case class DoctTerms(filename:String, terms:Map[String, Int]) 
 
-case class DoctTerms(filename:String, terms:Map[String, Int])
+class studyClass {
+  def tokenize(line: String): Array[String] = new WrappedString(line).toLowerCase().split(' ')
 
-def study(filename:String):DocTerms = {
-  val counts = (sc.textFile(filename).flatMap(tokenize).map((s:String) => (s,1))
-    .reduceByKey( _ + _ )
-    .collectAsMap())
-  DocTerms(filename, counts)
+  def study(filename:String):DocTerms = {
+    val conf = new SparkConf().setMaster("local[*").setAppName("simple-app")
+    val sc= new SparkContext(conf)
+
+    val counts = (sc.textFile(filename).flatMap(tokenize).map((s:String) => (s,1))
+      .reduceByKey( _ + _ )
+      .collectAsMap())
+    DocTerms(filename, counts)
+  }
 }
 
-val book1 = study("/home/cc/Highperformance_Computing/HW6/sherlockShort.txt")
+object Main {
 
-for(c<-book1.terms.slice(20)) println(c)
+  def main() {
+    val stc = new studyClass
+    val book1 = stc.study("/home/cc/Highperformance_Computing/HW6/sherlockShort.txt")
+
+    for(c<-book1.terms.slice(20)) println(c)
+  }
+
+}
